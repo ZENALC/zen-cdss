bf3a9122d267b60ff3130f18dc545fb23b6d98de
mihirshrestha <mshrestha@bostonlighthouse.us>
2021-08-01 10:49:58 -0400

    added missing docstrings

--- backend/tests/test_basic_vitals.py
+++ backend/tests/test_basic_vitals.py
@@ -20,6 +20,12 @@ from backend.formulas.basic_vitals import (get_homa_ir, get_bmi, get_blood_press
     ]
 )
 def test_get_homa_ir(insulin: float, glucose: float, expected: float):
+    """
+    Test the get HOMA IR function.
+    :param insulin: Insulin value of patient.
+    :param glucose: Glucose value of patient.
+    :param expected: Expected HOMA IR value.
+    """
     assert get_homa_ir(insulin=insulin, glucose=glucose) == expected
 
 
@@ -33,6 +39,12 @@ def test_get_homa_ir(insulin: float, glucose: float, expected: float):
     ]
 )
 def test_get_bmi(weight: float, height: float, expected: float):
+    """
+    Test the get BMI function.
+    :param weight: Weight of patient.
+    :param height: Height of patient.
+    :param expected: Expected BMI.
+    """
     assert get_bmi(weight=weight, height=height) == expected
 
 
@@ -46,6 +58,12 @@ def test_get_bmi(weight: float, height: float, expected: float):
     ]
 )
 def test_get_blood_pressure(systolic_bp: int, diastolic_bp: int, expected: str):
+    """
+    Test the get blood pressure function.
+    :param systolic_bp: Systolic blood pressure.
+    :param diastolic_bp: Diastolic blood pressure.
+    :param expected: Expected blood pressure.
+    """
     assert get_blood_pressure(systolic_bp=systolic_bp, diastolic_bp=diastolic_bp) == expected
 
 
@@ -59,6 +77,11 @@ def test_get_blood_pressure(systolic_bp: int, diastolic_bp: int, expected: str):
     ]
 )
 def test_get_maximum_heart_rate(age: int, expected: int):
+    """
+    Test the get maximum heart rate function.
+    :param age: Age of patient.
+    :param expected: Expected maximum heart rate.
+    """
     assert get_maximum_heart_rate(age=age) == expected
 
 
@@ -69,6 +92,11 @@ def test_get_maximum_heart_rate(age: int, expected: int):
     ]
 )
 def test_get_target_heart_rate(maximum_heart_rate: int, expected: str):
+    """
+    Test the get target heart rate function.
+    :param maximum_heart_rate: Maximum heart rate for patient.
+    :param expected: Expected target heart rate.
+    """
     assert get_target_heart_rate(maximum_heart_rate=maximum_heart_rate) == expected
 
 
@@ -82,6 +110,12 @@ def test_get_target_heart_rate(maximum_heart_rate: int, expected: str):
     ]
 )
 def test_get_tg_hdl_ratio(tg: int, hdl: int, expected: float):
+    """
+    Test the get TG/HDL ratio.
+    :param tg: Triglyceride value.
+    :param hdl: HDL value.
+    :param expected: Expected TG/HDL radio.
+    """
     assert get_tg_hdl_ratio(tg, hdl) == expected
 
 
@@ -95,6 +129,12 @@ def test_get_tg_hdl_ratio(tg: int, hdl: int, expected: float):
     ]
 )
 def test_get_triglyceride_glucose_index(tg: int, glucose: int, expected: float):
+    """
+    Test the get triglyceride glucose index function.
+    :param tg: Triglyceride value.
+    :param glucose: Glucose value.
+    :param expected: Expected triglyceride glucose index.
+    """
     assert get_triglyceride_glucose_index(tg=tg, glucose=glucose) == expected
 
 
@@ -108,6 +148,12 @@ def test_get_triglyceride_glucose_index(tg: int, glucose: int, expected: float):
     ]
 )
 def test_get_atherogenic_index_of_plasma(tg: int, hdl: int, expected: float):
+    """
+    Test the get atherogenic index of plasma function.
+    :param tg: Triglyceride value.
+    :param hdl: HDL value.
+    :param expected: Expected atherogenic index of plasma value.
+    """
     assert get_atherogenic_index_of_plasma(tg=tg, hdl=hdl) == expected
 
 
@@ -121,6 +167,12 @@ def test_get_atherogenic_index_of_plasma(tg: int, hdl: int, expected: float):
     ]
 )
 def test_get_body_adiposity_index(waist: int, height: int, expected: int):
+    """
+    Test the get body adiposity index function.
+    :param waist: Waist of patient.
+    :param height: Height of patient.
+    :param expected: Expected body adiposity index.
+    """
     assert get_body_adiposity_index(waist=waist, height=height) == expected
 
 
@@ -145,6 +197,15 @@ def test_get_body_adiposity_index(waist: int, height: int, expected: int):
 )
 def test_get_visceral_adiposity_index(gender: str, waist: int, tg: int, hdl: int, bmi: float,
                                       expected: Union[float, ValueError]):
+    """
+    Test the get visceral adiposity index formula.
+    :param gender: Gender of patient.
+    :param waist: Waist value.
+    :param tg: Triglyceride value.
+    :param hdl: HDL value.
+    :param bmi: BMI of patient.
+    :param expected: Expected visceral adiposity index formula.
+    """
     if expected == ValueError:
         with pytest.raises(ValueError, match=GENDER_ERROR_MESSAGE):
             get_visceral_adiposity_index(gender=gender, waist=waist, tg=tg, hdl=hdl, bmi=bmi)
@@ -172,6 +233,13 @@ def test_get_visceral_adiposity_index(gender: str, waist: int, tg: int, hdl: int
     ]
 )
 def test_get_lipid_accumulation_product(gender: str, waist: int, tg: int, expected: Union[ValueError, float]):
+    """
+    Test the get lipid accumulation production function.
+    :param gender: Gender of patient.
+    :param waist: Waist of patient.
+    :param tg: Triglyceride value.
+    :param expected: Expected lipid accumulation product value.
+    """
     if expected == ValueError:
         with pytest.raises(ValueError, match=GENDER_ERROR_MESSAGE):
             get_lipid_accumulation_product(gender=gender, waist=waist, tg=tg)
@@ -189,6 +257,14 @@ def test_get_lipid_accumulation_product(gender: str, waist: int, tg: int, expect
     ]
 )
 def test_get_fatty_liver_index(tg: int, ggt: int, waist: int, bmi: float, expected: int):
+    """
+    Test the get fatty liver index retrieval function.
+    :param tg: TG value.
+    :param ggt: GGT value.
+    :param waist: Waist value.
+    :param bmi: BMI of patient.
+    :param expected: Expected FLI.
+    """
     assert get_fatty_liver_index(tg=tg, ggt=ggt, waist=waist, bmi=bmi) == expected
 
 
@@ -202,6 +278,12 @@ def test_get_fatty_liver_index(tg: int, ggt: int, waist: int, bmi: float, expect
     ]
 )
 def test_get_waist_height_ratio(waist: int, height: int, expected: float):
+    """
+    Test get waist height ratio.
+    :param waist: Waist of patient.
+    :param height: Height of patient.
+    :param expected: Expected waist/height ratio.
+    """
     assert get_waist_height_ratio(waist=waist, height=height) == expected
 
 
@@ -215,6 +297,12 @@ def test_get_waist_height_ratio(waist: int, height: int, expected: float):
     ]
 )
 def test_get_alt_ast_ratio(alt: int, ast: int, expected: float):
+    """
+    Test get ALT/AST ratio.
+    :param alt: ALT.
+    :param ast: AST.
+    :param expected: Expected ratio.
+    """
     assert get_alt_ast_ratio(alt=alt, ast=ast) == expected
 
 
@@ -238,6 +326,13 @@ def test_get_alt_ast_ratio(alt: int, ast: int, expected: float):
     ]
 )
 def test_get_egfr(creatinine: float, gender: str, age: int, expected: Union[ValueError, int]):
+    """
+    Test the get EGFR function.
+    :param creatinine: Creatinine of patient.
+    :param gender: Gender of patient.
+    :param age: Age of patient.
+    :param expected: Expected EGFR.
+    """
     if expected == ValueError:
         with pytest.raises(ValueError, match=GENDER_ERROR_MESSAGE):
             get_egfr(creatinine=creatinine, gender=gender, age=age)
@@ -255,4 +350,9 @@ def test_get_egfr(creatinine: float, gender: str, age: int, expected: Union[Valu
     ]
 )
 def test_get_estimated_average_glucose(hba1c: float, expected: float):
+    """
+    Test the get estimated average glucose function.
+    :param hba1c: Hba1c value.
+    :param expected: Expected estimated average glucose.
+    """
     assert get_estimated_average_glucose(hba1c=hba1c) == expected
