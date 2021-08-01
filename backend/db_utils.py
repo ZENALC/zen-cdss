6f73dc26bb50c27023bbf84c9511eed8eacf8ac3
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-01 15:39:06 -0400

    added some files

---
+++ backend/db_utils.py
@@ -0,0 +1,3 @@
+"""
+File containing utilities for database operations.
+"""
---
+++ backend/main.py
@@ -0,0 +1,12 @@
+"""
+Main file for FastAPI.
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
