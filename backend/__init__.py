d2d36decfafa22c2bedf5616b6365270c5c0acac
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-06 22:01:04 -0400

    moving around

--- backend/__init__.py
+++ backend/__init__.py
@@ -10,6 +10,3 @@ ROOT_PATH = os.path.dirname(BACKEND_PATH)
 # TODO: Leverage PostgreSQL instead of SQLite.
 DB_NAME = "zen_cdss.db"
 DB_PATH = os.path.join(ROOT_PATH, DB_NAME)
-
-TEST_DB_NAME = "zen_cdss_test.db"
-TEST_DB_PATH = os.path.join(ROOT_PATH, TEST_DB_NAME)
--- backend/tests/database/__init__.py
+++ backend/tests/database/__init__.py
@@ -2,10 +2,15 @@
 Database testing initialization.
 """
 
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from backend import TEST_DB_PATH
+from backend import ROOT_PATH
+
+TEST_DB_NAME = "zen_cdss_test.db"
+TEST_DB_PATH = os.path.join(ROOT_PATH, TEST_DB_NAME)
 
 TEST_ENGINE = create_engine(f'sqlite:///{TEST_DB_PATH}')
 TEST_SESSION = sessionmaker(bind=TEST_ENGINE)
