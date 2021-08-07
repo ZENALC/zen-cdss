aa301cf2c1f6975843ff1febea1ff74a6a8be682
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-06 22:20:08 -0400

    add setup/teardown for testing insertions

--- backend/tests/database/test_insertions.py
+++ backend/tests/database/test_insertions.py
@@ -1,6 +1,24 @@
 """
 Testing insertions.
 """
+import os
+
+import backend.database.base as backend_base
+from backend.tests.database import TEST_DB_PATH, TEST_ENGINE
+
+
+def setup_module():
+    """
+    Setup test module for testing.
+    """
+    backend_base.Base.metadata.create_all(TEST_ENGINE)
+
+
+def teardown_module():
+    """
+    Teardown post testing.
+    """
+    os.remove(TEST_DB_PATH)
 
 
 def test_create_entry():
