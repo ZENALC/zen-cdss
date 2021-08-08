c5ba4d54c2dfb0a8e5d4a3222c2f9c0447c25ad4
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-08 18:40:25 -0400

    added some files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import {
+  Button, Form, FormControl, Nav, Navbar, NavDropdown,
+} from 'react-bootstrap';
+import React from 'react';
+
+function MainNavbar() {
+  return (
+    <Navbar expand="lg" variant="dark" bg="dark">
+      <Navbar.Brand href="#home">ZEN CDSS</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item>Action</NavDropdown.Item>
+            <NavDropdown.Item>Another action</NavDropdown.Item>
+            <NavDropdown.Item>Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Patient Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default MainNavbar;
