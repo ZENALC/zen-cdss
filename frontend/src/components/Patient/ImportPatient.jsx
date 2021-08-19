46ea744d0758ab21bedfee497e9fc6776013a027
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-18 22:15:13 -0400

    add better looking page to import view

--- frontend/src/components/Patient/ImportPatient.jsx
+++ frontend/src/components/Patient/ImportPatient.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function ImportPatient() {
   return (
     <>
       <br />
       <h3>Import Patient/Patients</h3>
+      <p>Please upload a CSV containing patient information to import patients.</p>
+      <hr />
+      <input type="file" />
+      <Button variant="success">Import</Button>
     </>
   );
 }
