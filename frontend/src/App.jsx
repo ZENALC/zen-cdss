f0969a6b6acb9846165dcbf6a93610f1a84bb791
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-18 22:00:56 -0400

    remove redundant stuff and disable annoying linebreak rule

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import MainNavbar from './components/Navbar';
-import logo from './logo.svg';
 import './App.css';
 import AddPatient from './components/Patient/AddPatient';
 import ViewPatient from './components/Patient/ViewPatient';
@@ -21,18 +20,6 @@ function App() {
           <Route exact path="/view_patient" component={ViewPatient} />
         </Switch>
       </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit src/App.jsx and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
